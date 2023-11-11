359d33550eb1533de017418bd2f2fb11b397d56f
Godfrey Karugu <karugugk@gmail.com>
2023-11-11 16:53:54 +0300

    Working With Multiple State Variables

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -35,14 +35,66 @@ function handleLiClick(id){
   setFoods(newFoodsArray);
 }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id} onClick={()=> handleLiClick(food.id)}>
-      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
-    </li>
-  ));
+// -----------------------------Working With Multiple State Variables---------------------------------------
+ 
+
+
+
+<select name="filter">
+  <option value="All">All</option>
+  <option value="American">American</option>
+  <option value="Mexican">Mexican</option>
+  <option value="Thai">Thai</option>
+  <option value="Sichuan">Sichuan</option>
+</select>
+
+const [filterBy, setFilterBy] = useState("All");
+
+function handleFilterChange(e){
+  setFilterBy(e.target.value)
+}
+
+const foodsToDisplay = foods.filter((food) =>{
+  if(filterBy === "All"){
+    return true;
+  }else {
+    return food.cuisine === filterBy;
+  }
+
+})
+
+// const foodsToDisplay = foods.filter((food) => {
+//   if (filterBy === "All") {
+//     return true;
+//   } else {
+//     return food.cuisine === filterBy;
+//   }
+// });
+
+
+
+
+const foodList = foodsToDisplay.map((food) => (
+  <li key={food.id} onClick={()=> handleLiClick(food.id)}>
+    {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+  </li>
+));
+// --------------------------------------------------------------------------------------------
+  // const foodList = foods.map((food) => (
+  //   <li key={food.id} onClick={()=> handleLiClick(food.id)}>
+  //     {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+  //   </li>
+  // ));
 
   return (
     <div>
+    <select name="filter" onChange={handleFilterChange}>
+          <option value="All">All</option>
+          <option value="American">American</option>
+          <option value="Mexican">Mexican</option>
+          <option value="Thai">Thai</option>
+          <option value="Sichuan">Sichuan</option>
+      </select>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
     </div>
